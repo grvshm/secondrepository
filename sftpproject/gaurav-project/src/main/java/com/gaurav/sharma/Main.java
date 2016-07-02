3e32dc92443ab78771d8132bc9f34637614d0511
gaurav sharma <gaurav sharma>
2016-07-02 16:43:15 +0530

    Proper First Commit

---
+++ sftpproject/gaurav-project/src/main/java/com/gaurav/sharma/Main.java
@@ -0,0 +1,8 @@
+package com.gaurav.sharma;
+
+public class Main {
+ public String getName(){
+	 return "gaurav";
+ }
+
+}
---
+++ sftpproject/gaurav-project/src/test/java/com/gaurav/sharma/MainTest.java
@@ -0,0 +1,12 @@
+package com.gaurav.sharma;
+
+import junit.framework.TestCase;
+
+public class MainTest extends TestCase{
+  
+	
+	public void testmain(){
+		Main main = new Main();
+	assertEquals("gaurav",main.getName());
+	}
+}
